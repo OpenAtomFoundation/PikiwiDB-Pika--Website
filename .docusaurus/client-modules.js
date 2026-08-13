d56c84fb8685afa2eb9f01ba039d7c73aec6813b
unknown <wangyingyi@360.cn>
2026-08-13 17:57:54 +0800

    clear build things

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/wenyi/project/website/node_modules/infima/dist/css/default/default.css"),
-  require("/home/wenyi/project/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/wenyi/project/website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/wenyi/project/website/src/css/custom.css"),
-];
--- build/assets/js/a7456010.fee74d50.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkPikiwiDB_Pika_=self.webpackChunkPikiwiDB_Pika_||[]).push([[235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
